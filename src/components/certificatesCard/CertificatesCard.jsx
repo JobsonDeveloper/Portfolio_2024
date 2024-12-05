7a4a6213a246d8704310c819a2f6d70dea849b8e
Jobson Oliveira <jobsondeveloper@gmail.com>
2024-12-04 21:09:28 -0300

    Campo de certificados finalizado. Foi adicionado o título e a foto para Hard Skills

--- src/components/certificatesCard/CertificatesCard.jsx
+++ src/components/certificatesCard/CertificatesCard.jsx
@@ -5,9 +5,19 @@ import { Zoom } from "@mui/material";
 export const CertificatesCard = (props) => {
     return (
         <Zoom in={props.showCertificate}>
-            <div className="jd_certificates_data">
-                <img src={props.certificateUrl} alt="Foto de um certificado" className="jd_certificates_img" />
-            </div>
+            <a
+                href="https://drive.google.com/drive/folders/16KZQidhcQKFhNzc8YW4JycDC4RhdeUcc?usp=sharing"
+                className="jd_certificates_link"
+                target="_blank"
+            >
+                <div className="jd_certificates_data">
+                    <img
+                        src={props.certificateUrl}
+                        alt="Foto de um certificado"
+                        className="jd_certificates_img"
+                    />
+                </div>
+            </a>
         </Zoom>
     )
 }
--- src/views/knowledge/Knowledge.jsx
+++ src/views/knowledge/Knowledge.jsx
@@ -5,13 +5,36 @@ import 'aos/dist/aos.css';
 import { Header } from "../../components/header/Header";
 import { Footer } from "../../components/footer/Footer";
 import { Loading } from "../../components/loading/Loading";
+import { Pagination } from "@mui/material";
 
 // Backgrounds
 import backgroundCertificates from '../../assets/images/backgroundCertificados.svg';
 import backgroundHardSkills from '../../assets/images/backgroundHardSkills.svg';
+import { CertificatesCard } from "../../components/certificatesCard/CertificatesCard";
+
+// Certificates
+import DioReactAndTypeScript from '../../assets/certificates/DIO-Paginas-com-React-e-TypeScript.jpg';
+import DioJavaScriptDebuggingError from '../../assets/certificates/DIO-Debugging-e-Error-Handling-JavaScript.jpg';
+import DioJavaScriptFunctions from '../../assets/certificates/DIO-Dominando-Funcoes-JavaScript.jpg';
+import DioIntroductFomGitAndGitHub from '../../assets/certificates/DIO-Introducao-ao-Git-e-GitHub.jpg';
+import DioOrientationFromObject from '../../assets/certificates/DIO-Orientacao-a-Objetos-com-JavaScript.jpg';
+import UBUnibraLogicFromProgramming from '../../assets/certificates/UNIBRA-Intencivo-de-Logica-de-Programacao-Java.jpg';
+import UBProjectOrientedObject from '../../assets/certificates/UNIBRA-Projeto-com-Programacao-Orientada-a-Objetos.jpg';
+import UBManagementProject from '../../assets/certificates/UNIBRA-Gestao-de-Projetos.jpg';
 
 export const Knowledge = () => {
     const [loading, setLoading] = useState(true);
+    const [certificatePage, setCertificatePage] = useState(1);
+    const [showCertificate, setShowCertificate] = useState(true);
+
+    const changeCertificatePage = (event, newPage) => {
+        setShowCertificate(false);
+
+        setTimeout(() => {
+            setCertificatePage(newPage);
+            setShowCertificate(true);
+        }, 500);
+    };
 
     useEffect(() => {
         Aos.init();
@@ -39,6 +62,83 @@ export const Knowledge = () => {
                         <article className="jd_sections_images" data-aos="zoom-in">
                             <img
                                 src={backgroundCertificates}
+                                alt="Desenho de pessoa olhando um caminho"
+                                className='jd_sections_images_background'
+                                data-aos="zoom-in"
+                            />
+                        </article>
+
+                        <article className="jd_main_certificates">
+                            {certificatePage === 1 &&
+                                <CertificatesCard
+                                    certificateUrl={DioReactAndTypeScript}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+                            {certificatePage === 2 &&
+                                <CertificatesCard
+                                    certificateUrl={DioJavaScriptDebuggingError}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+                            {certificatePage === 3 &&
+                                <CertificatesCard
+                                    certificateUrl={DioJavaScriptFunctions}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+                            {certificatePage === 4 &&
+                                <CertificatesCard
+                                    certificateUrl={DioIntroductFomGitAndGitHub}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+                            {certificatePage === 5 &&
+                                <CertificatesCard
+                                    certificateUrl={DioOrientationFromObject}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+                            {certificatePage === 6 &&
+                                <CertificatesCard
+                                    certificateUrl={UBUnibraLogicFromProgramming}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+                            {certificatePage === 7 &&
+                                <CertificatesCard
+                                    certificateUrl={UBProjectOrientedObject}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+                            {certificatePage === 8 &&
+                                <CertificatesCard
+                                    certificateUrl={UBManagementProject}
+                                    showCertificate={showCertificate}
+                                />
+                            }
+
+                            <article className="jd_certificate_pagination">
+                                <Pagination
+                                    count={8}
+                                    page={certificatePage}
+                                    onChange={changeCertificatePage}
+                                    hidePrevButton
+                                    hideNextButton
+                                />
+                            </article>
+                        </article>
+                    </li>
+
+                    <li className="jd_sections_itens">
+                        <article className="jd_sections_titles jd_title_right" data-aos="fade-left">
+                            <h2 className="jd_item_title">Hard Skills</h2>
+                            <p className="jd_item_subtitle">Tecnologias conhecidas</p>
+                        </article>
+
+                        <article className="jd_sections_images" data-aos="zoom-in">
+                            <img
+                                src={backgroundHardSkills}
                                 alt="Desenho de pessoa olhando um caminho"
                                 className='jd_sections_images_background'
                                 data-aos="zoom-in"
